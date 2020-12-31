a2bcd1e09add628e79b7de29a2ab309c76cec862
YamashitaMount <wavewalkerz23@gmail.com>
2020-12-31 20:12:03 +0900

    Update day2.vue

--- pages/day2.vue
+++ pages/day2.vue
@@ -1,7 +1,8 @@
 <template>
 <p>傷心気味で一日中PCにむかってJSを書いていた。<br>
 気分が落ち着いたら、参考にさせていただいたリンク集でもまとめよう<br>
-あと、掌田先生のvue.js本ほしいな</p>
+あと、掌田先生のvue.js本ほしいな</br>
+良いお年を</p>
 </template>
 <script>
 export default {layout: 'sample'}
